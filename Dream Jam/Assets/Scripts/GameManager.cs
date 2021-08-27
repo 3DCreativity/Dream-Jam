158392984fe051a840a2a939dcb158544cd94aaa
3D Creativity <3d.creativity.design@gmail.com>
2021-08-28 00:47:58 +0300

    Finished Main Menu
    Finished the main menu

---
+++ Dream Jam/Assets/Scripts/GameManager.cs
@@ -0,0 +1,60 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class GameManager : MonoBehaviour
+{
+    public GameObject MainMenu;
+    public GameObject ControllsMenu;
+    public GameObject CreditsMenu;
+    public Animator animator;
+    public UnityEvent StartConversation;
+
+    private void Awake()
+    {
+        if (StartConversation == null)
+        {
+            StartConversation = new UnityEvent();
+        }
+    }
+
+    public void DisplayControllsMenu()
+    {
+        MainMenu.SetActive(false);
+        ControllsMenu.SetActive(true);
+    }
+    public void DisplayCreditsMenu()
+    {
+        MainMenu.SetActive(false);
+        CreditsMenu.SetActive(true);
+    }
+    public void ExitGame()
+    {
+        Debug.Log("Quit");
+        Application.Quit();
+    }
+    public void ReturnController()
+    {
+        ControllsMenu.SetActive(false);
+        MainMenu.SetActive(true);
+    }
+    public void ReturnCredits()
+    {
+        CreditsMenu.SetActive(false);
+        MainMenu.SetActive(true);
+    }
+    public void ActionStart()
+    {
+        StartCoroutine(StartAnimation());
+    }
+
+    IEnumerator StartAnimation()
+    {
+        animator.SetTrigger("isPlaying");
+        yield return new WaitForSeconds(1);
+        MainMenu.SetActive(false);
+        StartConversation.Invoke();
+    }
+
+}
